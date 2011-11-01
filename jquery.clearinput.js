a87612228a29441b98390b90d5abc4d833d98d9a
Aidan Feldman <aidan.feldman@gmail.com>
2011-11-01 01:28:35 -0400

    minor refactoring of setting logic

--- jquery.clearinput.js
+++ jquery.clearinput.js
@@ -2,18 +2,15 @@
 jQuery Clear-Input plugin
 v1.0
 Author: Aidan Feldman
-
-USAGE
-Add the class .clear-input to any text input element
-whose value you want cleared when it gains focus.
-The initial value will be replaced when the input
-loses focus, and no new text has been entered.
-
-If you prefer to not add classes to your elements,
-you can alternatively call clearInput() on any jQuery
-input object.
+Site: http://github.com/afeld/jquery-clearinput
 */
 (function( $ ){
+  // sets the field to the empty-value if it has none
+  function setIfEmpty(el){
+    var $el = $(el);
+    $el.val() || $el.val($el.data('empty-value'));
+  }
+  
   $.fn.clearInput = function(emptyValue){
     return this
       .each(function(){
@@ -22,8 +19,7 @@ input object.
       
         // store the value as a data property
         $el.data('empty-value', eltEmptyVal);
-        // initialize the field if it has no value
-        $el.val() || $el.val(eltEmptyVal);
+        setIfEmpty($el);
       })
       .focus(function(){
         var $el = $(this);
@@ -32,10 +28,7 @@ input object.
         }
       })
       .blur(function(){
-        var $el = $(this);
-        if (!$el.val()){
-          $el.val($el.data('empty-value'));
-        }
+        setIfEmpty(this);
       });
   };
 })( jQuery );
